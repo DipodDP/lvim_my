d6aa74735e17aef59e630901c50c741e8599dc8e
Kaique Moreira da Silva <kaique.moreira.dev@gmail.com>
2023-08-07 13:23:06 -0300

    add markdon preview plugins

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -182,7 +182,6 @@ which_key.register(mappings, opts)
 which_key.register(vmappings, vopts)
 which_key.register(vmappings, vopts)
 
-error("this is something")
 vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)"
 vim.cmd "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)"
 vim.cmd "command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()"

b93d1a2450805f010248d2083d0fb3129a981b08
dp <dipoddp@gmail.com>
2024-02-21 16:20:38 +0700

    Init commit

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,40 +1,57 @@
-local status, jdtls = pcall(require, "jdtls")
-if not status then
+-- credit: https://github.com/ChristianChiarulli/nvim
+local status_ok, jdtls = pcall(require, "jdtls")
+if not status_ok then
   return
 end
 
--- Setup Workspace
-local home = os.getenv "HOME"
-local workspace_path = home .. "/.local/share/lunarvim/jdtls-workspace/"
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-local workspace_dir = workspace_path .. project_name
-
 -- Determine OS
-local os_config = "linux"
+local home = vim.env.HOME
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason")
+local launcher_path = vim.fn.glob(mason_path .. "/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")
+if #launcher_path == 0 then
+  launcher_path = vim.fn.glob(mason_path .. "/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar", true, true)[1]
+end
+local CONFIG = "linux"
 if vim.fn.has "mac" == 1 then
-  os_config = "mac"
+  WORKSPACE_PATH = home .. "/workspace/"
+  CONFIG = "mac"
+elseif vim.fn.has "unix" == 1 then
+  WORKSPACE_PATH = home .. "/workspace/"
+else
+  vim.notify("Unsupported system", vim.log.levels.ERROR)
 end
 
--- Setup Capabilities
-local capabilities = require("lvim.lsp").common_capabilities()
+-- Find root of project
+local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+local root_dir = require("jdtls.setup").find_root(root_markers)
+if root_dir == "" then
+  return
+end
+
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
 
-local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = WORKSPACE_PATH .. project_name
 
--- Setup Testing and Debugging
-local bundles = {}
--- vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar",true), "\n"))
-vim.list_extend(bundles, vim.split(vim.fn.glob("/home/moreira/.local/share/nvim/vscode-java-test/server/*.jar",true), "\n"))
-vim.list_extend(
-  bundles,
-  vim.split(
-    vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",true),
-    "\n"
+-- Test bundle
+-- Run :MasonInstall java-test
+local bundles = { vim.fn.glob(mason_path .. "/packages/java-test/extension/server/*.jar", true) }
+if #bundles == 0 then
+  bundles = { vim.fn.glob(mason_path .. "/packages/java-test/extension/server/*.jar", true) }
+end
+-- Debug bundle
+-- Run :MasonInstall java-debug-adapter
+local extra_bundles =
+  vim.fn.glob(mason_path .. "/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar", true)
+if #extra_bundles == 0 then
+  extra_bundles = vim.fn.glob(
+    mason_path .. "/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+    true
   )
-)
+end
+vim.list_extend(bundles, { extra_bundles })
 
-lvim.builtin.dap.active = true
 local config = {
   cmd = {
     "java",
@@ -43,24 +60,40 @@ local config = {
     "-Declipse.product=org.eclipse.jdt.ls.core.product",
     "-Dlog.protocol=true",
     "-Dlog.level=ALL",
+    "-javaagent:" .. mason_path .. "/packages/jdtls/lombok.jar",
     "-Xms1g",
     "--add-modules=ALL-SYSTEM",
     "--add-opens",
     "java.base/java.util=ALL-UNNAMED",
     "--add-opens",
     "java.base/java.lang=ALL-UNNAMED",
-    "-javaagent:" .. mason_path .. "/packages/jdtls/lombok.jar",
     "-jar",
-    vim.fn.glob(mason_path .. "/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+    launcher_path,
     "-configuration",
-    mason_path .. "/packages/jdtls/config_" .. os_config,
+    mason_path .. "/packages/jdtls/config_" .. CONFIG,
     "-data",
     workspace_dir,
   },
-  root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" },
-  capabilities = capabilities,
+
+  on_attach = function(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+    if lvim.builtin.dap.active then
+      require("jdtls.dap").setup_dap_main_class_configs()
+      require("jdtls").setup_dap { hotcodereplace = "auto" }
+      require("lvim.lsp").on_attach(client, bufnr)
+    end
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  on_exit = require("lvim.lsp").common_on_exit,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  root_dir = root_dir,
   settings = {
     java = {
+      -- jdt = {
+      --   ls = {
+      --     vmargs = "-XX:+UseParallelGC -XX:GCTimeRatio=4 -XX:AdaptiveSizePolicyWeight=90 -Dsun.zip.disableMemoryMapping=true -Xmx1G -Xms100m"
+      --   }
+      -- },
       eclipse = {
         downloadSources = true,
       },
@@ -97,13 +130,41 @@ local config = {
       format = {
         enabled = true,
         settings = {
-          -- profile = "GoogleStyle",
-          -- url = home .. "/.config/lvim/.java-google-formatter.xml",
+          profile = "GoogleStyle",
+          url = home .. "/.config/lvim/.java-google-formatter.xml",
         },
       },
     },
     signatureHelp = { enabled = true },
+    completion = {
+      favoriteStaticMembers = {
+        "org.hamcrest.MatcherAssert.assertThat",
+        "org.hamcrest.Matchers.*",
+        "org.hamcrest.CoreMatchers.*",
+        "org.junit.jupiter.api.Assertions.*",
+        "java.util.Objects.requireNonNull",
+        "java.util.Objects.requireNonNullElse",
+        "org.mockito.Mockito.*",
+      },
+    },
+    contentProvider = { preferred = "fernflower" },
     extendedClientCapabilities = extendedClientCapabilities,
+    sources = {
+      organizeImports = {
+        starThreshold = 9999,
+        staticStarThreshold = 9999,
+      },
+    },
+    codeGeneration = {
+      toString = {
+        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+      },
+      useBlocks = true,
+    },
+  },
+  flags = {
+    allow_incremental_sync = true,
+    server_side_fuzzy_completion = true,
   },
   init_options = {
     bundles = bundles,
